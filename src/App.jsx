6279d9e0d9707cf6eec6d6901739a9c23e09777d
SatvikDB <satvikdb04@gmail.com>
2026-07-28 17:48:54 +0530

    feat: add Vercel Web Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <Analytics />
     </div>
   );
 }
